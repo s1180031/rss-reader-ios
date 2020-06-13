aa49ede9031aa019c14a7207b0fafd29dbe14684
Yoshitaka Hashimoto <65984749+s1180031@users.noreply.github.com>
2020-06-13 15:10:49 +0900

    initial commit

---
+++ RSSReader/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RSSReader
+//
+//  Created by yhashimoto on 2020/06/13.
+//  Copyright © 2020 s1180031. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
